e2bc46a6509cfd10e4f27344d88837607400e209
FWTP <138397080+FWTPKOM@users.noreply.github.com>
2026-08-31 14:39:05 +0200

    Create dane.js

---
+++ dane.js
@@ -0,0 +1,79 @@
+```javascript
+const linie = [
+
+    // =====================
+    // AUTOBUSY
+    // =====================
+
+    {
+        numer: "101",
+        typ: "autobus",
+        nazwa: "Linia autobusowa 101"
+    },
+
+    {
+        numer: "102",
+        typ: "autobus",
+        nazwa: "Linia autobusowa 102"
+    },
+
+    {
+        numer: "103",
+        typ: "autobus",
+        nazwa: "Linia autobusowa 103"
+    },
+
+    {
+        numer: "104",
+        typ: "autobus",
+        nazwa: "Linia autobusowa 104"
+    },
+
+    {
+        numer: "105",
+        typ: "autobus",
+        nazwa: "Linia autobusowa 105"
+    },
+
+
+    // =====================
+    // TRAMWAJE
+    // =====================
+
+    {
+        numer: "1",
+        typ: "tramwaj",
+        nazwa: "Linia tramwajowa 1"
+    },
+
+    {
+        numer: "2",
+        typ: "tramwaj",
+        nazwa: "Linia tramwajowa 2"
+    },
+
+    {
+        numer: "9",
+        typ: "tramwaj",
+        nazwa: "Linia tramwajowa 9"
+    },
+
+
+    // =====================
+    // METRO
+    // =====================
+
+    {
+        numer: "M1",
+        typ: "metro",
+        nazwa: "Linia metra M1"
+    },
+
+    {
+        numer: "M2",
+        typ: "metro",
+        nazwa: "Linia metra M2"
+    }
+
+];
+```
